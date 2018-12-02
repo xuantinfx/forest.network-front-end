b49b070c7abf1cc27c90a37ee024cc54455d11ba
Xuân Tin <xuantinfx@gmail.com>
2018-12-02 23:20:12 +0700

    map data từ reducer vào

--- src/views/TinTest.js
+++ src/views/TinTest.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 // import PropTypes from 'prop-types'
 import Navbar from '../components/Navbar/index';
-import Banner from '../components/Banner/index'
+import Banner from '../containers/Banner'
 
 export default class TinTest extends Component {
     static propTypes = {
@@ -14,15 +14,7 @@ export default class TinTest extends Component {
                 <Navbar />
                 <div id="page-outer">
                     <div id="page-container" className="AppContent">
-                        <Banner 
-                            urlImg="./template/1500x500"
-                            tweet="23 N"
-                            followings="1.018"
-                            follower="71,1 Tr"
-                            likes="2.316"
-                            list="1"
-                            moment="4"
-                        />
+                        <Banner />
                     </div>
                 </div>
             </div >
