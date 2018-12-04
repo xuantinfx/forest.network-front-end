53395149f980e096ebdbb9b604861af69d94512f
Xuân Tin <xuantinfx@gmail.com>
2018-12-04 15:00:03 +0700

    layout pofile

--- src/views/TinTest.js
+++ src/views/TinTest.js
@@ -7,7 +7,7 @@ import ProfileSidebar from '../containers/ProfileSidebar'
 
 export default class TinTest extends Component {
     static propTypes = {
-        
+
     }
 
     render() {
@@ -17,8 +17,21 @@ export default class TinTest extends Component {
                 <div id="page-outer">
                     <div id="page-container" className="AppContent">
                         <Banner />
-                        <Profile></Profile>
-                        <ProfileSidebar></ProfileSidebar>
+                        <div class="AppContainer">
+                            <div class="AppContent-main content-main u-cf" role="main" aria-labelledby="content-main-heading">
+                                <div class="Grid Grid--withGutter">
+                                    <Profile></Profile>
+                                    <div class="Grid-cell u-size2of3 u-lg-size3of4">
+                                        <div class="Grid Grid--withGutter">
+                                            <div class="Grid-cell">
+                                                <div class="js-profileClusterFollow"></div>
+                                            </div>
+                                            <ProfileSidebar></ProfileSidebar>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div >
