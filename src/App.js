71ef8c1b49f92b6acb9f5ff50a8ee81489a0b986
Dong-Nhan <ddnn97@hotmail.com>
2018-12-04 18:10:53 +0700

    route 2 page Follow và Profile

--- src/App.js
+++ src/App.js
@@ -1,14 +1,44 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
-import TinTest from './views/TinTest';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Banner from './containers/Banner'
+import Profile from './containers/Profile'
+import Follow from './containers/Follow'
+import ProfileTimeline from './components/ProfileTimeline';
 
 class App extends Component {
   render() {
     return (
-      <TinTest></TinTest>
+      <Router>
+        <div>
+          <Navbar />
+          <div id="page-outer">
+            <div id="page-container" className="AppContent">
+              <Banner />
+              <div className="AppContainer">
+                <div className="AppContent-main content-main u-cf" role="main" aria-labelledby="content-main-heading">
+                  <div className="Grid Grid--withGutter">
+                    <Profile></Profile>
+                    <div className="Grid-cell u-size2of3 u-lg-size3of4">
+                      <div className="Grid Grid--withGutter">
+                        <Switch>
+                          <Route exact path="/followers" component={Follow} />
+                          <Route exact path="/" component={ProfileTimeline} />
+                        </Switch>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div >
+      </Router>
     );
   }
 }
+
 
 export default App;
