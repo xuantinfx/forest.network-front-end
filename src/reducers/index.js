76d7adb585e0b058fc34cc5b82a0941b47dd0c48
Xuân Tin <xuantinfx@gmail.com>
2018-12-04 20:29:06 +0700

    thêm reducer vào index reducer, khá nhiều bug, mấy bác merge về fix từ từ nha(cái nào lỗi dis nó ra)

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,12 @@
 import { combineReducers } from 'redux'
+import followers from './followerReducer'
+import followings from './followingReducer'
+import profile from './profileReducer'
+import tweets from './tweetReducer'
 
-
-
-
-export default combineReducers({})
+export default combineReducers({
+    followers,
+    followings,
+    profile,
+    tweets
+})
