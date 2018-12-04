34e4ad39ba01cf99f6d7d475de52fc75c5ffa544
Vi Van <vivanpro1401@gmail.com>
2018-12-04 21:46:18 +0700

    fixed profile

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react'
 import ProfileHeaderCard from './ProfileHeaderCard';
-import PhotoRail from './PhotoRail';
+//import PhotoRail from './PhotoRail';
 
-export class Profile extends Component {
+class Profile extends Component {
     render() {
         return (
             <div className="Grid-cell u-size1of3 u-lg-size1of4">
@@ -10,10 +10,11 @@ export class Profile extends Component {
                     <div className="Grid-cell">
                         <div className="ProfileSidebar ProfileSidebar--withLeftAlignment">
                             <div className='ProfileHeaderCard'>
-                                <ProfileHeaderCard name={this.props.name} bio={this.props.bio} location={this.props.location}
-                                    hashtag={this.props.hashtag} url={this.props.url} joinDate={this.props.joinDate}></ProfileHeaderCard>
+                                <ProfileHeaderCard displayName={this.props.displayName} userName={this.props.userName} 
+                                    bio={this.props.bio} location={this.props.location}
+                                    joinDate={this.props.joinDate}></ProfileHeaderCard>
                                 {/* <PhotoRail withCountLink={this.props.withCountLink} withCountNum={this.props.withCountNum}
-                        mediaBoxImg={this.props.mediaBoxImg}></PhotoRail>       */}
+                        mediaBoxImg={this.props.mediaBoxImg}></PhotoRail>*/}
                             </div>
                         </div>
                     </div>
