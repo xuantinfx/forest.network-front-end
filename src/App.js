6313ea1ed9a8e5a4c4eeee1dda712a3657b95c38
Vi Van <vivanpro1401@gmail.com>
2018-12-04 17:40:56 +0700

    fixed conflict

--- src/App.js
+++ src/App.js
@@ -2,12 +2,11 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import TinTest from './views/TinTest';
-import ProfileEdit from './components/ProfileEdit/ProfileEdit';
 
 class App extends Component {
   render() {
     return (
-      <ProfileEdit></ProfileEdit>
+      <TinTest></TinTest>
     );
   }
 }
