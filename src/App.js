84bd78b76d95f7fca6c73b29481f55390e6fe51e
Dong-Nhan <ddnn97@hotmail.com>
2018-12-04 16:45:12 +0700

    tách ProfileTimeline và TimelineTweet

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import TinTest from './views/TinTest'
+import ProfileTimeline from './components/ProfileTimeline';
 
 class App extends Component {
   render() {
     return (
-      <TinTest/>
+      <ProfileTimeline/>
     );
   }
 }
