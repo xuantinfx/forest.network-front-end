2d39d6954b64264dacba5ea1fb60ac7a5b60204b
Xuân Tin <xuantinfx@gmail.com>
2018-12-04 17:36:27 +0700

    add component follower and followwing

--- src/views/TinTest.js
+++ src/views/TinTest.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 import Navbar from '../components/Navbar/index';
 import Banner from '../containers/Banner'
 import Profile from '../containers/Profile'
-import ProfileSidebar from '../containers/ProfileSidebar'
+import Follow from '../containers/Follow'
 
 export default class TinTest extends Component {
     static propTypes = {
@@ -17,18 +17,11 @@ export default class TinTest extends Component {
                 <div id="page-outer">
                     <div id="page-container" className="AppContent">
                         <Banner />
-                        <div class="AppContainer">
-                            <div class="AppContent-main content-main u-cf" role="main" aria-labelledby="content-main-heading">
-                                <div class="Grid Grid--withGutter">
+                        <div className="AppContainer">
+                            <div className="AppContent-main content-main u-cf" role="main" aria-labelledby="content-main-heading">
+                                <div className="Grid Grid--withGutter">
                                     <Profile></Profile>
-                                    <div class="Grid-cell u-size2of3 u-lg-size3of4">
-                                        <div class="Grid Grid--withGutter">
-                                            <div class="Grid-cell">
-                                                <div class="js-profileClusterFollow"></div>
-                                            </div>
-                                            <ProfileSidebar></ProfileSidebar>
-                                        </div>
-                                    </div>
+                                    <Follow/>
                                 </div>
                             </div>
                         </div>
