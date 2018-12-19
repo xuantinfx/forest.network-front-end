39a580a8a483ad8aa3c2b5808f83bf1b5ba36f34
Dong-Nhan <ddnn97@hotmail.com>
2018-12-19 11:47:22 +0700

    hoàn thành component

--- src/components/Banner/LogoUser.jsx
+++ src/components/Banner/LogoUser.jsx
@@ -3,43 +3,50 @@ import UploadImageModal from "../UploadImageModal";
 // import PropTypes from 'prop-types'
 
 export default class LogoUser extends Component {
-    constructor(props) {
-        super(props)
-
-        this.state = {
-            modal: {
-                isOpen: false
-            }
-        }
+  constructor(props) {
+    super(props)
+    this.state = {
+      modal: {
+        isOpen: false
+      },
+      imgBinaryData: null
     }
+  }
 
-    changeProfileAvatar = () => {
-        alert('click');
+  _changeProfileAvatar = (file) => {
+    let fileReader = new FileReader();
+    fileReader.readAsBinaryString(file);
+    fileReader.onload = (e) => {
+      this.setState({ imgBinaryData: e.target.result });
     }
+  }
 
-    toggleModal = () => {
-        console.log('toggle')
-        this.setState((prevState) => {
-            const prevIsOpen = prevState.modal.isOpen;
-            console.log(prevIsOpen);
-            return { modal: { isOpen: !prevIsOpen } }
-        });
-    }
+  _toggleModal = () => {
+    this.setState((prevState) => {
+      const prevIsOpen = prevState.modal.isOpen;
+      return { modal: { isOpen: !prevIsOpen } }
+    });
+  }
 
-    render() {
-        return (
-            <Fragment>
-                <div className="AppContainer">
-                    <div className="ProfileCanopy-avatar" onClick={this.toggleModal}>
-                        <div className="ProfileAvatar">
-                            <div className="ProfileAvatar-container u-block js-tooltip profile-picture" >
-                                <img className="ProfileAvatar-image " src={`data:image/jpeg;base64${btoa(this.props.imgBinaryData)}`} alt="YouTube" />
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <UploadImageModal isOpen={this.state.modal.isOpen} toggle={this.toggleModal} />
-            </Fragment>
-        )
-    }
+  render() {
+    return (
+      <Fragment>
+        <div className="AppContainer">
+          <div className="ProfileCanopy-avatar" onClick={this._toggleModal}>
+            <div className="ProfileAvatar">
+              <div className="ProfileAvatar-container u-block js-tooltip profile-picture" >
+                <img className="ProfileAvatar-image"
+                  src={this.state.imgBinaryData
+                    ? `data:image/jpeg;base64,${btoa(this.state.imgBinaryData)}`
+                    : '/img/picturenotfound.png'}
+                  alt="Name" />
+              </div>
+            </div>
+          </div>
+        </div>
+        <UploadImageModal isOpen={this.state.modal.isOpen} toggle={this._toggleModal}
+          existedImgData={this.state.imgBinaryData} sendImage={this._changeProfileAvatar} />
+      </Fragment>
+    )
+  }
 }
--- src/components/UploadImageModal/index.js
+++ src/components/UploadImageModal/index.js
@@ -1,34 +1,89 @@
 import React, { Component } from 'react';
-import { Modal, ModalHeader, ModalBody, ModalFooter, Row, Col } from 'reactstrap';
+import { Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import PropTypes from 'prop-types';
 import Dropzone from 'react-dropzone';
 
 export default class index extends Component {
-  constructor() {
-    super()
+  static propTypes = {
+    isOpen: PropTypes.bool.isRequired,
+    toggle: PropTypes.func.isRequired,
+    existedImgData: PropTypes.string,
+    sendImage: PropTypes.func
+  }
+
+  constructor(props) {
+    super(props)
     this.state = {
+      files: [],
+      existedImgData: this.props.existedImgData
     }
+  }
+
+  shouldComponentUpdate(nextProps) {
+    if (nextProps.existedImgData !== this.state.existedImgData) {
+      this.setState({
+        existedImgData: nextProps.existedImgData
+      })
+    }
+    return true;
+  }
+
+  _onDrop = (acceptedFiles, rejectedFiles) => {
+    if (rejectedFiles.length > 0) {
+      alert('File không hợp lệ');
+      return;
+    }
+    this.setState({ files: acceptedFiles });
+
+    //change image inside modal first
+    this._changeImageInside(acceptedFiles[0]);
+
+  }
+
+  _changeImageInside = (file) => {
+    let fileReader = new FileReader();
+    try {
+      fileReader.readAsBinaryString(file);
+      fileReader.onload = (e) => {
+        this.setState({ existedImgData: e.target.result });
+      }
+    }
+    catch (err) {
+      console.error(err);
+      alert('File không hợp lệ')
+    }
+  }
+
+  _sendImage = () => {
+    this.props.sendImage(this.state.files[0]);
   }
 
   render() {
     return (
       <Modal isOpen={this.props.isOpen} toggle={this.props.toggle} className={this.props.className}>
-        <ModalHeader toggle={this.props.toggle}>Modal title</ModalHeader>
-        <ModalBody>
-          <Dropzone
+        <ModalHeader toggle={this.props.toggle}>Cập nhật ảnh đại diện</ModalHeader>
+        <ModalBody className="justify-content-center">
+          <Dropzone multiple={false}
             accept="image/jpeg, image/png"
-            onDrop={(accepted, rejected) => { this.setState({ accepted, rejected }); }}>
+            onDrop={this._onDrop}>
             {({ getRootProps, getInputProps }) => (
               <div {...getRootProps()} className="dropzone">
                 <input {...getInputProps()} />
-                <p>Try dropping some files here, or click to select files to upload.</p>
-                <p>Only *.jpeg and *.png images will be accepted</p>
+                <div className="ProfileAvatar mx-auto">
+                  <div className="ProfileAvatar-container u-block js-tooltip profile-picture" >
+                    <img className="ProfileAvatar-image" alt="Click để chọn ảnh"
+                      src={this.state.existedImgData
+                        ? `data:image/jpeg;base64,${btoa(this.state.existedImgData)}`
+                        : '/img/picturenotfound.png'} />
+                  </div>
+                </div>
               </div>
             )}
           </Dropzone>
         </ModalBody>
         <ModalFooter>
           <button className="btn primary-btn"
-            onClick={this.props.sendFile}>Cập nhật</button>
+            onClick={this._sendImage}>Cập nhật</button>
           <button className="btn"
             onClick={this.props.toggle}>Hủy</button>
         </ModalFooter>
