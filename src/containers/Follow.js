f3c5802b06304192275bcfc23624e46b0f99b9c1
Xuân Tin <xuantinfx@gmail.com>
2018-12-20 23:20:09 +0700

    update

--- src/containers/Follow.js
+++ src/containers/Follow.js
@@ -5,6 +5,9 @@ import { getFollowing } from '../apis/following';
 import { getFollower } from '../apis/follower';
 import { beginLoadFollower, loadFollowerDone } from '../actions/followerActions';
 import { beginLoadFollowing, loadFollowingDone } from '../actions/followingActions'
+import { followings } from '../lib/encodeTX';
+import { postTranSaction } from '../apis/transaction'
+import _ from 'lodash';
 
 const mapStateToProps = (state, ownProps) => {
 
@@ -18,6 +21,19 @@ const mapStateToProps = (state, ownProps) => {
         listFollow = state.followings.followings || [];
         isLoading = state.followings.isLoading;
     }
+
+    listFollow = _.map(listFollow, profile => {
+        if(_.findIndex(state.user.followings, folowing => profile.address === folowing) >= 0) {
+            return {
+                ...profile,
+                isFollow: true,
+            }
+        }
+        return {
+            ...profile,
+            isFollow: false,
+        }
+    })
 
     return {
         listFollow,
@@ -51,13 +67,26 @@ const loadFollow = (dispatch, address) => {
     }
 }
 
+const updateFollowings = (listFollowings, sequence) => {
+    let secretKey = window.localStorage.getItem("PRIVATE_KEY");
+    requestApi(postTranSaction(followings(secretKey, sequence, Buffer.alloc(0), listFollowings, 1)))
+    .then(res => {
+        console.log('res', res);
+    })
+    .catch(err => {
+        console.log(err);
+    })
+}
+
 const mapDispatchToProps = (dispatch, ownProps) => {
     return {
         loadFollow: (address) => {
-            loadFollow(dispatch, address)
+            loadFollow(dispatch, address);
+        },
+        updateFollowings: (listFollowings, sequence) => {
+            updateFollowings(listFollowings, sequence);
         }
     }
 }
-
 
 export default connect(mapStateToProps, mapDispatchToProps)(Follow)
