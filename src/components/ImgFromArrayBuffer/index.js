4190c68a7a960c71b274cac5fefe67111e62afef
Xuân Tin <xuantinfx@gmail.com>
2018-12-22 16:37:13 +0700

    hotfix preview picture

--- src/components/ImgFromArrayBuffer/index.js
+++ src/components/ImgFromArrayBuffer/index.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import _ from 'lodash';
 
 export default class index extends Component {
   static propTypes = {
@@ -11,9 +10,13 @@ export default class index extends Component {
   }
 
   render() {
-    let imgSrc = !_.isEmpty(this.props.arrayBufferData)
+    let imgSrc = this.props.arrayBufferData
       ? `data:image/jpeg;base64,${Buffer.from(this.props.arrayBufferData).toString('base64')}`
       : '/img/picturenotfound.png';
+
+      if(this.props.arrayBufferData && this.props.arrayBufferData.length === 0) {
+        imgSrc = '/img/picturenotfound.png';
+      }
 
     return (
       <img className={this.props.className} style={this.props.style} alt={this.props.alt || ''} src={imgSrc} />
