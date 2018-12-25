4377cf9cc53a0bcfd34b383480cd6f8b75145b1f
Xuân Tin <xuantinfx@gmail.com>
2018-12-25 16:46:56 +0700

    hotfix khong save block

--- src/lib/socket/decodeSocket.js
+++ src/lib/socket/decodeSocket.js
@@ -2,6 +2,7 @@ import typeSocket from '../../constants/typeSocket';
 import { followingPost, receiveFollowings, receiveUnfollowings, receiveReaction, receiveComment, receiveMoney} from '../../actions/socketAction';
 
 export default (dispatch, obj) => {
+    console.log('obj', obj);
     switch (obj.type) {
         case typeSocket.FOLLOWING_POST:
             dispatch(followingPost(obj.address, obj.name, obj.content));
