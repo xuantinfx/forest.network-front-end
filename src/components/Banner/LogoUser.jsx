e6264b44a26d9b821b7db7e86f30b706c4cf6ceb
Dong-Nhan <ddnn97@hotmail.com>
2018-12-27 11:28:22 +0700

    hotfix resize image

--- src/components/Banner/LogoUser.jsx
+++ src/components/Banner/LogoUser.jsx
@@ -2,7 +2,8 @@ import React, { Component, Fragment } from 'react';
 import UploadImageModal from "../UploadImageModal";
 import ImgFromArrayBuffer from '../ImgFromArrayBuffer';
 import PropTypes from 'prop-types'
-import { INVALID_IMAGE_ERROR_MESSAGE } from '../../constants/Image';
+import Jimp from 'jimp';
+import { INVALID_IMAGE_ERROR_MESSAGE, MAX_IMAGE_SIZE } from '../../constants/Image';
 
 export default class LogoUser extends Component {
   static propTypes = {
@@ -23,13 +24,38 @@ export default class LogoUser extends Component {
     }
   }
 
-  _changeProfileAvatar = (file) => {
+  _changeProfileAvatar = async (file) => {
+    if (!file) {
+      this._toggleModal();
+      return;
+    }
     let fileReader = new FileReader();
+    let isLargeFile = file.size > (MAX_IMAGE_SIZE / 2);
+    this.props.beginUpdateProfilePicture();
     try {
       fileReader.readAsArrayBuffer(file);
-      fileReader.onload = (e) => {
+      fileReader.onload = async (e) => {
         let result = Buffer.from(e.target.result);
-        this.props.updateProfilePicture(result);
+        if (isLargeFile) {
+          console.log('LARGE IMAGE FILE: ', result);
+          try {
+            //read image to jimp
+            let image = await Jimp.read(result);
+            //resize image
+            image.resize(100, Jimp.AUTO);
+            //get buffer result
+            result = await image.getBufferAsync(Jimp.MIME_JPEG);
+            console.log('SIZE-REDUCED IMAGE FILE: ', result);
+
+            //finally, update
+            this.props.updateProfilePicture(result);
+          }
+          catch (err) {
+            console.log(err);
+            this.props.showError('Xảy ra lỗi')
+          }
+        }
+        else this.props.updateProfilePicture(result);
       }
     } catch (err) {
       this.props.showError(INVALID_IMAGE_ERROR_MESSAGE);
